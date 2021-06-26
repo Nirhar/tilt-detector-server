29b9d071601278992fa9f179a00719269c9657ba
Nirhar <manishkausikhari@gmail.com>
2021-06-26 22:44:12 +0530

    Server Done

--- main/server.cpp
+++ main/server.cpp
@@ -80,8 +80,8 @@ void setup_wifi(){
     };
     wifi_config_t wifi_config = {
         .sta = {
-            {.ssid = "HARI"},
-            {.password = "vu2hkevu2hkevu2hke"},
+            {.ssid = CONFIG_EXAMPLE_WIFI_SSID},
+            {.password = CONFIG_EXAMPLE_WIFI_PASSWORD},
             /* Setting a password implies station will connect to all security modes including WEP/WPA.
              * However these modes are deprecated and not advisable to be used. Incase your Access point
              * doesn't support WPA2, these mode can be enabled by commenting below line */
